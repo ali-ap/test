4aac256dd4b531602bfc06c0863e9dbf83233222
Ali Alipour <ali_alipour@live.com>
2020-07-11 00:32:17 +0200

    Dtos validation added

---
+++ Material/LUM.Services.Material/Validation/CreateMaterialBindingModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using LUM.Services.Material.Model.Request;
+
+namespace LUM.Services.Material.Validation
+{
+    public class CreateMaterialBindingModelValidator : AbstractValidator<CreateMaterialBindingModel>
+    {
+        public CreateMaterialBindingModelValidator()
+        {
+            RuleFor(x => x.FunctionMinTemperature).GreaterThanOrEqualTo(4).LessThanOrEqualTo(80);
+            RuleFor(x => x.FunctionMaxTemperature).GreaterThanOrEqualTo(4).LessThanOrEqualTo(80);
+        }
+    }
+}
---
+++ Material/LUM.Services.Material/Validation/UpdateMaterialBindingModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LUM.Services.Material.Model.Request;
+
+namespace LUM.Services.Material.Validation
+{
+    public class UpdateMaterialBindingModelValidator : AbstractValidator<UpdateMaterialBindingModel>
+    {
+        public UpdateMaterialBindingModelValidator()
+        {
+            RuleFor(x => x.FunctionMinTemperature).GreaterThanOrEqualTo(4).LessThanOrEqualTo(80);
+            RuleFor(x => x.FunctionMaxTemperature).GreaterThanOrEqualTo(4).LessThanOrEqualTo(80);
+        }
+    }
+}
