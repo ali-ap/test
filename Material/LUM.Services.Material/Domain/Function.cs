4110e4c2680e3e80722690ae9889a11c8cffaae3
Ali Alipour <ali_alipour@live.com>
2020-07-11 00:28:41 +0200

    material aggregate and related domains are added

---
+++ Material/LUM.Services.Material/Domain/Function.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using LUM.Services.Material.Type;
+
+namespace LUM.Services.Material.Domain
+{
+    public class Function:ValueObject
+    {
+        public Temperature MinTemperature { get; set; }
+        public Temperature MaxTemperature { get; set; }
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return MinTemperature.Celsius;
+            yield return MaxTemperature.Celsius;
+        }
+    }
+}
